3d8001d48624c13a95ca8d07772e961d52ca4ffa
Hani  Murtaja <35417117+HaniMurtaja@users.noreply.github.com>
2020-11-19 15:07:22 +0200

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,41 @@
+<?php return array (
+  'backpack/base' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Backpack\\Base\\BaseServiceProvider',
+    ),
+  ),
+  'backpack/generators' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Backpack\\Generators\\GeneratorsServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'jenssegers/date' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Jenssegers\\Date\\DateServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Date' => 'Jenssegers\\Date\\Date',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+);
