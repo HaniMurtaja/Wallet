487c376f85511595a96bf72146f7f5b74b8b6b35
Hani  Murtaja <35417117+HaniMurtaja@users.noreply.github.com>
2020-11-21 01:13:25 +0200

    Add files via upload

---
+++ database/migrations/create_admins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('names');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('dob');
+            $table->decimal('Total_expenses', 13, 4);
+            $table->decimal('Total_income', 13, 4);
+            $table->string('Registered_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
