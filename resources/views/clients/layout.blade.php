012753e3fb84f487d004e1ad38b80c2d274d612e
Hani  Murtaja <35417117+HaniMurtaja@users.noreply.github.com>
2020-11-20 19:33:59 +0200

    Update layout.blade.php

--- resources/views/clients/layout.blade.php
+++ resources/views/clients/layout.blade.php
@@ -209,26 +209,6 @@
         <!-- start:footer -->
         <div class="footer-bottom">
             <div class="container">
-                <div class="footer-bottom-widget">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <p>
-                            <span class="sosmed-footer">
-                                <a href="#"><i class="fa fa-facebook" data-toggle="tooltip" data-placement="top" title="Facebook"></i></a>
-                                <a href="#"><i class="fa fa-twitter" data-toggle="tooltip" data-placement="top" title="Twitter"></i></a>
-                            
-                            </span>
-                            &copy; 2018 <strong>e</strong>-Wallet<strong>.</strong></p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p class="footer-bottom-links">
-                                <a href="#">About <strong>e</strong>-Wallet<strong>.</strong></a>
-                                <a href="#">Privacy Policy</a>
-                                <a href="#">Terms & Conditions</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         <!-- end:footer -->
@@ -293,5 +273,4 @@
 
 </body>
 
-<!-- Mirrored from bootemplates.com/themes/srikandi/v2/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Dec 2017 16:15:27 GMT -->
 </html>	
